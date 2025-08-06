a5665eb4d5fcaaa1af17e643faef4e9b8f00c489
PaciFisting <lex.n.99@bk.ru>
2025-08-06 22:15:59 +0500

    on_third_step

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,4 +1,6 @@
 import argparse
+import json
+import os
 
 
 def main():
@@ -6,10 +8,14 @@ def main():
         prog='gendiff',
         description='Compares two configuration files and shows a difference.'
         )
-    parser.add_argument('first_file', help=None)
-    parser.add_argument('second_file', help=None)
+    parser.add_argument('first_file', help='file1.json')
+    parser.add_argument('second_file', help='file2.json')
     parser.add_argument('-f', '--format', help='set format of output', default='stylish')
     parser.print_help()
 
+    json.load(open('С:\\Python\\python-project-50\\gendiff\\scripts\\file1.json'))
+    json.load(open('С:\\Python\\python-project-50\\gendiff\\scripts\\file2.json'))
+
 if __name__ == "__main__":    
     main()
+
