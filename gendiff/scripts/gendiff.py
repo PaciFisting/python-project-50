159e924dd1cda632cd06f75c0cb8e5cc365cfd74
PaciFisting <lex.n.99@bk.ru>
2025-08-06 17:26:36 +0500

    second_step

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -2,10 +2,13 @@ import argparse
 
 
 def main():
-    parser = argparse.ArgumentParser(prog='gendiff')
-    parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
+    parser = argparse.ArgumentParser(
+        prog='gendiff',
+        description='Compares two configuration files and shows a difference.'
+        )
     parser.add_argument('first_file', help=None)
     parser.add_argument('second_file', help=None)
+    parser.add_argument('-f', '--format', help='set format of output', default='stylish')
     parser.print_help()
 
 if __name__ == "__main__":    
