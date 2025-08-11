5e342b702740641799382b9dec018ec4387d8c33
PaciFisting <lex.n.99@bk.ru>
2025-08-11 22:59:16 +0500

    on_fifth_step

--- Makefile
+++ Makefile
@@ -5,3 +5,6 @@ build:
 .PHONY: gendiff
 gendiff:
 	uv run gendiff file1.json file2.json
+
+lint:
+	uv run ruff check gendiff --fix
--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,5 +1,5 @@
-import json
 import argparse
+import json
 
 
 def generate_diff(first_dict, second_dict):
@@ -16,7 +16,9 @@ def generate_diff(first_dict, second_dict):
     for key in second_dict:
         if key not in first_dict:
             diff_dict[f'  + {key}'] = second_dict[key]
-    sorted_diff = dict(sorted(diff_dict.items(), key=lambda x: x[0].lstrip('+- ')))
+    sorted_diff = dict(
+        sorted(diff_dict.items(), key=lambda x: x[0].lstrip('+- '))
+        )
     return sorted_diff
 
 
@@ -26,9 +28,15 @@ def sort_diff_dict(diff_dict):
 
 
 def main():
-    parser = argparse.ArgumentParser(description='Compares two JSON files and shows the difference.')
-    parser.add_argument('first_file', type=str, help='Path to the first JSON file')
-    parser.add_argument('second_file', type=str, help='Path to the second JSON file')
+    parser = argparse.ArgumentParser(
+        description='Compares two JSON files and shows the difference.'
+        )
+    parser.add_argument(
+        'first_file', type=str, help='Path to the first JSON file'
+        )
+    parser.add_argument(
+        'second_file', type=str, help='Path to the second JSON file'
+        )
     args = parser.parse_args()
 
     first_dict = json.load(open(args.first_file))
